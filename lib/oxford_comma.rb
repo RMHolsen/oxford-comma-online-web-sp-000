76719e83d27e5271648e3623d702e2630b34802e
Rosario Holsen <rholsen@gmail.com>
2020-08-17 19:35:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   if array.length < 3 
-    puts array.join(" and ")
+    return array.join(" and ")
   elsif 
     zoidberg = array.last
     array.pop
-    puts array.join(", ") + ", and #{zoidberg}"
+    return array.join(", ") + ", and #{zoidberg}"
   end 
 end
